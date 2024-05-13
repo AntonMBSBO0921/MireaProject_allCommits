521d0dabe5dc3066c777beffa83c1818ccea0310
AntonMBSBO0921 <ttttr1991@gmail.com>
2024-05-14 02:47:04 +0300

    small fixes

--- MireaProject/app/src/main/java/ru/mirea/maiorovaa/mireaproject/fragment_camera.java
+++ MireaProject/app/src/main/java/ru/mirea/maiorovaa/mireaproject/fragment_camera.java
@@ -4,45 +4,46 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
-
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 public class fragment_camera extends Fragment {
 
+    private ImageView imageView;
+    private Button captureButton;
+
     private static final int REQUEST_IMAGE_CAPTURE = 1;
-    private Button takePhotoButton;
-    private ImageView photoPreview;
 
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_camera, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_camera, container, false);
 
-        takePhotoButton = root.findViewById(R.id.take_photo_button);
-        photoPreview = root.findViewById(R.id.photo_preview);
+        imageView = view.findViewById(R.id.imageView);
+        captureButton = view.findViewById(R.id.captureButton);
 
-        takePhotoButton.setOnClickListener(new View.OnClickListener() {
+        captureButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 dispatchTakePictureIntent();
             }
         });
 
-        return root;
+        return view;
     }
 
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        }
+        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
     }
 
     @Override
@@ -50,10 +51,9 @@ public class fragment_camera extends Fragment {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK && data != null) {
             Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            if (imageBitmap != null) {
-                photoPreview.setImageBitmap(imageBitmap);
-                photoPreview.setVisibility(View.VISIBLE);
+            if (extras != null) {
+                Bitmap imageBitmap = (Bitmap) extras.get("data");
+                imageView.setImageBitmap(imageBitmap);
             }
         }
     }
